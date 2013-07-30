306f52134abedbfed22be38627420628f79d3a6f
genevy <genevy@genevy-Inspiron-1545>
2013-07-30 18:05:03 -0300

    fazendo commit no arquivo index.php, colocando o die para ver aonde ta o error da tela em branco

--- public/index.php
+++ public/index.php
@@ -3,10 +3,15 @@
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
  */
+
+die('aqui estou fazendo um teste');
+
 chdir(dirname(__DIR__));
 
 // Setup autoloading
+
 require 'init_autoloader.php';
 
 // Run the application!
+
 Zend\Mvc\Application::init(require 'config/application.config.php')->run();
